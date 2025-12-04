e59a1496a78c35d001a9fa0defad22ddd4e305d0
Ahmed Salah Eldin <ahmedsalahghobish@hotmail.com>
2025-12-04 12:43:27 +0200

    playwright setup

---
+++ WebUIAutomation/playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+export default defineConfig({
+  testDir: './tests/specs',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    
+    baseURL: 'https://www.automationexercise.com/',
+
+    trace: 'on-first-retry',
+    screenshot: 'on',
+    video: 'on',
+    headless: false,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
